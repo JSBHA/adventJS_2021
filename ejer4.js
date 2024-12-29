77b347861fd93ee2c460f288761ac0ea53202014
JSBHA <jhoan-071995@hotmail.com>
2024-12-29 12:49:38 -0500

    Exercise 4 is completed by counting the days until Christmas and after the 25th.

--- ejer4.js
+++ ejer4.js
@@ -1,19 +1,20 @@
 function createXmasTree(height) {
 
-    let tree = [];
+    let tree = []; // Se crea un arreglo vacio 
 
-    for (let i = 1; i <= height; i++){
-        let leftSpace = '_'.repeat(height - i);
-        let asteriscos = '*'.repeat(2 * i - 1);
-        let filaTree = `${leftSpace}${asteriscos}${leftSpace}`;
+        // Crear las filas del árbol
+       for (let i = 1; i <= height; i++){
+        let leftSpace = '_'.repeat(height - i); // Disminuye los espacios con cada iteracion 
+        let asteriscos = '*'.repeat(2 * i - 1); // Aumenta los asteriscos con cada iteracion 
+        let filaTree = `${leftSpace}${asteriscos}${leftSpace}`; // Se concatena los string 
 
-        tree.push(filaTree)
+        tree.push(filaTree) // Añadir la fila al array
 
     }
 
     
-    let trunk = '_'.repeat(height - 1) + '#' + '_'.repeat(height - 1)
-    tree.push(trunk);
+    let trunk = '_'.repeat(height - 1) + '#' + '_'.repeat(height - 1)  // Tronco con los espacios adecuados
+    tree.push(trunk); // se agrega 2 veces para que el tronco tenga 2 pisos 
     tree.push(trunk);
   
     return tree.join('\n');
