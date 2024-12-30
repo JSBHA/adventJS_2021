222c910d2815b801f88120bc5e216d6bdfa3bac2
JSBHA <jhoan-071995@hotmail.com>
2024-12-29 23:02:20 -0500

    Challenge 6 is completed where we have to add 2 elements of an array to reach a result that is already given to us

---
+++ ejer6.js
@@ -0,0 +1,24 @@
+function sumPairs(numbers, result) {
+    const seen = {}; // Objeto para almacenar los números ya vistos
+
+    for (let i = 0; i < numbers.length; i++) {
+        const num = numbers[i];
+        const complement = result - num;
+
+        // Si el complemento ya se vio antes, devolvemos el par
+        if (seen[complement] !== undefined) {
+            return [complement, num];
+        }
+
+        // Agregamos el número actual al objeto `seen`
+        seen[num] = i; // Guardamos el índice para mayor control (aunque no es necesario en esta implementación)
+    }
+
+    return null; // Si no se encuentra ningún par
+}
+
+  console.log(sumPairs([3, 5, 7, 2], 10)); // [3, 7]
+  console.log(sumPairs([-3, -2, 7, -5], 10)) // null
+  console.log(sumPairs([2, 2, 3, 1], 4))//[2,2]
+  console.log(sumPairs([6, 7, 1, 2], 8))//[7, 1]
+  console.log(sumPairs([0, 2, 2, 3, -1, 1, 5], 6))
