08044aae24d8c4986c9bc35bb3ffe8f03d9b7b09
JSBHA <jhoan-071995@hotmail.com>
2024-12-29 23:12:07 -0500

    Challenge 7 is completed where we go through an object looking for a product and if we find it, return true, otherwise return false.

---
+++ ejer7.js
@@ -0,0 +1,50 @@
+function contains(store, product) {
+    
+    for (let key in store) {
+      const value = store[key];
+      if (typeof value === 'object') {
+        // Si el valor es un objeto, buscamos recursivamente
+        if (contains(value, product)) {
+          return true;
+        }
+      } else if (value === product) {
+        // Si encontramos el producto, devolvemos true
+        return true;
+      }
+    }
+    // Si no se encuentra el producto, devolvemos false
+    return false;
+  }
+  
+  // Ejemplos de uso
+  const almacen = {
+    'estanteria1': {
+      'cajon1': {
+        'producto1': 'coca-cola',
+        'producto2': 'fanta',
+        'producto3': 'sprite'
+      }
+    },
+    'estanteria2': {
+      'cajon1': 'vacio',
+      'cajon2': {
+        'producto1': 'pantalones',
+        'producto2': 'camiseta' // <- ¡Está aquí!
+      }
+    }
+  };
+  
+  console.log(contains(almacen, 'camiseta')); // true
+  
+  const otroAlmacen = {
+    'baul': {
+      'fondo': {
+        'objeto': 'cd-rom',
+        'otro-objeto': 'disquette',
+        'otra-cosa': 'mando'
+      }
+    }
+  };
+  
+  console.log(contains(otroAlmacen, 'gameboy')); // false
+  
