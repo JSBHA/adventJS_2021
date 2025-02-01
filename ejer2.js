5d4e745ff161ca489ada05a88e94003de4dad963
JSBHA <jhoan-071995@hotmail.com>
2025-02-01 16:32:52 -0500

    Challenge 2 is completed where we must organize Santa's letter by eliminating the spaces and the crossed out gifts.

--- ejer2.js
+++ ejer2.js
@@ -1,25 +1,18 @@
+function listGifts(letter) {
+  let arrGifts = letter.split(" "); // se combierte una lista en una arreglo
+
+  let giftsList = arrGifts
+    .map((gift) => gift.trim())
+    .filter((gift) => gift && !gift.startsWith("_")); // Se eliminan los espacios del arreglo y se eliman las palabras con caracter
+
+  let contador = {};
+
+  giftsList.forEach((gift) => {
+    contador[gift] = (contador[gift] || 0) + 1; // Se cuentas la cantidad de regaslos
+  });
+
+  return contador;
+}
+const regalos = listGifts(carta);
 const carta = 'bici coche balón _playstation bici coche peluche'
-
-  function listGifts(letter) {
-    let arrGifts = letter.split(' '); // se combierte una lista en una arreglo
-  
-    let giftsList = arrGifts
-    .map(gift => gift.trim())
-    .filter(gift => gift && !gift.startsWith('_')); // Se eliminan los espacios del arreglo y se eliman las palabras con caracter 
-  
-    let contador = {};
-  
-    giftsList.forEach(gift => {
-      (contador[gift] = (contador[gift] || 0) + 1 )// Se cuentas la cantidad de regaslos  
-    });
-    
-   return contador
-  console.log(contador)
-
-  
-  }
-
-
-  const regalos = listGifts(carta)
-
-  console.log(regalos)
+console.log(regalos);
