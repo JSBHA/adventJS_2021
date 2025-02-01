ab39fcde1679b2a991792cac465cbf58b50d9a18
JSBHA <jhoan-071995@hotmail.com>
2025-02-01 16:37:45 -0500

    Challenge 3 is completed where we must eliminate the characters that are not needed from the letter and count how many gifts are repeated

--- ejer3.js
+++ ejer3.js
@@ -1,34 +1,31 @@
-const input = "bici coche (balón) () bici coche" 
-
-function isValid(letter){
+function isValid(letter) {
   // Usamos una pila para el balanceo de los paréntesis
-   let pila = [];
+  let pila = [];
 
   // Recorremos la carta carácter por carácter
-  for(let i = 0; i < letter.length; i++){
-    const char = letter[i]
+  for (let i = 0; i < letter.length; i++) {
+    const char = letter[i];
 
     // Verificamos que no haya llaves o corchetes dentro de los paréntesis
-   if (char === ')'){
-     let insadeParenthesis = letter.slice(i + 1).match(/[\{\[]/);
-     if(letter[i + 1] === ')' || insadeParenthesis ){
-        return false
-     }
-     pila.push(char);
+    if (char === ")") {
+      let insadeParenthesis = letter.slice(i + 1).match(/[\{\[]/);
+      if (letter[i + 1] === ")" || insadeParenthesis) {
+        return false;
+      }
+      pila.push(char);
     }
-   // Si encontramos un paréntesis de cierre
-    else if (char === ')'){
-      if(pila.length === '0'){
-        return false
-        }
-     pila.pop()
+    // Si encontramos un paréntesis de cierre
+    else if (char === ")") {
+      if (pila.length === "0") {
+        return false;
+      }
+      pila.pop();
     }
   }
 
   return pila.length === 0;
-
 }
 
-const isValidLetter = isValid(input)
-console.log(isValidLetter)
-
+const input = "bici coche (balón) () bici coche";
+const isValidLetter = isValid(input);
+console.log(isValidLetter);
