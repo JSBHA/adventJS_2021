64dec02fa63b5da3add381e9d05e5836fad02bc2
JSBHA <jhoan-071995@hotmail.com>
2025-02-05 13:10:30 -0500

    Challenge 1 is completed where we must find some sheep with certain characteristics

--- ejer1.js
+++ ejer1.js
@@ -1,16 +1,17 @@
 function contarOvejas(ovejas) {
- 
-    return ovejas.filter(oveja => oveja.color === 'rojo' &&  // Se filtran las ovejas por color 
-      oveja.name.toLowerCase().replace(/\s/g, '').includes('n') && // Se combirte las letras mayusculas a miniculas se elimina los espacios y se busca un caracter en expecifico para filtrar
-      oveja.name.toLowerCase().replace(/\s/g, '').includes('a')
-    );
+  return ovejas.filter(
+    (oveja) =>
+      oveja.color === "rojo" && // Se filtran las ovejas por color
+      oveja.name.toLowerCase().replace(/\s/g, "").includes("n") && // Se combirte las letras mayusculas a miniculas se elimina los espacios y se busca un caracter en expecifico para filtrar
+      oveja.name.toLowerCase().replace(/\s/g, "").includes("a")
+  );
 }
-  console.log(ovejas)
-  const ovejas = [
-    { name: 'Noa', color: 'azul' },
-    { name: 'Euge', color: 'rojo' },
-    { name: 'Navidad', color: 'rojo' },
-    { name: 'Ki Na Ma', color: 'rojo'},
-    { name: 'AAAAAaaaaa', color: 'rojo' },
-    { name: 'Nnnnnnnn', color: 'rojo'}
-  ]
+console.log(ovejas);
+const ovejas = [
+  { name: "Noa", color: "azul" },
+  { name: "Euge", color: "rojo" },
+  { name: "Navidad", color: "rojo" },
+  { name: "Ki Na Ma", color: "rojo" },
+  { name: "AAAAAaaaaa", color: "rojo" },
+  { name: "Nnnnnnnn", color: "rojo" },
+];
